f4cb2887ba3c623f68fc368a2e44097e517ac283
Nowaj36 <nowajchowdhury36@gmail.com>
2025-11-25 14:15:52 +0600

    remove cors

--- src/main.ts
+++ src/main.ts
@@ -9,10 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://appifylab-backend-nest-production.up.railway.app',
-    ],
+    origin: ['http://localhost:3000'],
     credentials: true,
   });
 
